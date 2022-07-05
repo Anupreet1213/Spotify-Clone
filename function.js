da85417702fe2e799993f23d6041e8b31aac8a58
Anupreet1213 <93524594+Anupreet1213@users.noreply.github.com>
2022-07-06 00:40:53 +0530

    Update function.js

--- function.js
+++ function.js
@@ -4,6 +4,7 @@ var allSongs = {
   music: ["Lover - Diljit Dosanjh", "Pasoori - Shae Gill, Ali Sethi", "Bijlee - Harrdy Sandhu", "Excuses - Ap Dhillon", "Insane - Ap Dhillon"]
 }
 let audio = new Audio("Songs/1.mp3");
+let index = 0;
 
 $("#mainButt").click(function(){
   if(audio.paused || audio.currentTime<=0){
@@ -23,6 +24,19 @@ $("#mainButt").click(function(){
 audio.addEventListener("timeupdate", ()=>{
   progress = parseInt((audio.currentTime/audio.duration)*100);
   toggleBar.value = progress;
+  if(toggleBar.value==100){
+    if(index>=5){
+      index = 1;
+    }
+    else{
+      index += 1;
+    }
+    audio.src = ("Songs/"+(index)+".mp3");
+    audio.play();
+    document.getElementById("sideName").innerText = allSongs.music[index-1];
+    animate = ("#" + index);
+    $(animate).fadeIn(100).fadeOut(100).fadeIn(100);
+  }
 });
 
 toggleBar.addEventListener("click",()=>{
