46a5a1ce8c33e11e232e533cb59d7f50edb0c4ac
Anupreet1213 <93524594+Anupreet1213@users.noreply.github.com>
2022-10-20 12:06:14 +0530

    Update function2.js

--- function2.js
+++ function2.js
@@ -1,3 +1,4 @@
+let songIndex = 0;
 let toggleBar = document.getElementById("toggleBar");
 var allSongs = {
   music: ["Tum Se Hi - Mohit Chauhan", "Kaise Hua - Vishal Mishra", "Tera Ban Jaunga", "Ajab Si - KK", "Tera Hone - Atif Aslam"]
@@ -5,9 +6,25 @@ var allSongs = {
 let audio = new Audio("Songs1/1.mp3");
 let index = 0;
 
+Array.from(document.getElementsByClassName("songItem")).forEach((element)=>{
+  element.addEventListener("click", (e)=>{
+    index = parseInt(e.target.id);
+    audio.src = ("Songs1/"+(index)+".mp3");
+    document.getElementById("sideName").innerText = allSongs.music[index-1];
+    audio.currentTime = 0;
+    audio.play();
+    $("#mainButt").removeClass("fa-regular fa-2x fa-circle-play");
+    $("#mainButt").addClass("fa-regular fa-2x fa-circle-pause");
+    $("#gif").css("opacity", "1");
+    animate = ("#" + index);
+    $(animate).fadeIn(100).fadeOut(100).fadeIn(100);
+  });
+});
+
 $("#mainButt").click(function(){
   if(audio.paused || audio.currentTime<=0){
     audio.play();
+    console.log("hello");
     $("#mainButt").removeClass("fa-regular fa-2x fa-circle-play");
     $("#mainButt").addClass("fa-regular fa-2x fa-circle-pause");
     $("#gif").css("opacity", "1");
@@ -42,20 +59,6 @@ toggleBar.addEventListener("click",()=>{
   audio.currentTime = toggleBar.value * audio.duration/100;
 });
 
-Array.from(document.getElementsByClassName("songItem")).forEach((element)=>{
-  element.addEventListener("click", (e)=>{
-    index = parseInt(e.target.id);
-    audio.src = ("Songs1/"+(index)+".mp3");
-    document.getElementById("sideName").innerText = allSongs.music[index-1];
-    audio.currentTime = 0;
-    audio.play();
-    $("#mainButt").removeClass("fa-regular fa-2x fa-circle-play");
-    $("#mainButt").addClass("fa-regular fa-2x fa-circle-pause");
-    $("#gif").css("opacity", "1");
-    animate = ("#" + index);
-    $(animate).fadeIn(100).fadeOut(100).fadeIn(100);
-  });
-});
 $("#forw").click(()=>{
   if(index>=5){
     index = 1;
